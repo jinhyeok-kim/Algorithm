273ea6653bdfeee9577df6c1966120da6e6c897d
jinhyeok <persona5173@gmail.com>
2018-05-21 16:20:48 +0900

    boj 2302 complete & add 2032 readme

--- Java/_2302_TheaterSeat/src/Main.java
+++ Java/_2302_TheaterSeat/src/Main.java
@@ -5,32 +5,36 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int T = scan.nextInt();
-        for (int t = 0; t < T; t++) {
-            int N = scan.nextInt();
-            int [] plusNum = new int [N+1];
+        int N = scan.nextInt();
+        int[] seat = new int[N + 1];
 
-            if(N>3) {
-                plusNum[1] = 1;
-                plusNum[2] = 2;
-                plusNum[3] = 4;
+        int M = scan.nextInt();
+        int[] vip = new int[M + 1];
 
-                for (int i = 4; i < plusNum.length; i++) {
-                    plusNum[i] = plusNum[i - 1] + plusNum[i - 2] + plusNum[i - 3];
+        int num = 0;
+
+        for (int m = 0; m < M; m++) {
+            vip[m] = scan.nextInt();
+        }
+        seat[0] = 1;
+        seat[1] = 1;
+
+
+        for (int i = 1; i < seat.length; i++) {
+            if (vip[num] == i) {
+                seat[i] = seat[i - 1];
+            } else if (vip[num] == i - 1) {
+                seat[i] = seat[i - 1];
+                if(num < M) {
+                    num++;
+                }
+            } else {
+                if (i >= 2) {
+                    seat[i] = seat[i - 1] + seat[i - 2];
                 }
             }
-            else if(N == 1){
-                plusNum[1] = 1;
-            }
-            else if(N == 2){
-                plusNum[2] = 2;
-            }
-            else if(N == 3){
-                plusNum[3] = 4;
-            }
-
-            System.out.println(plusNum[N]);
-
         }
+
+        System.out.println(seat[N]);
     }
 }
