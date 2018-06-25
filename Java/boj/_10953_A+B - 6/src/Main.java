7da0f97070e6e9baa0d54570259f496df2c55289
jinhyeok <persona5173@gmail.com>
2018-06-25 23:37:25 +0900

    Complete: java boj 10953 A+B - 6

--- Java/boj/_10953_A+B - 6/src/Main.java
+++ Java/boj/_10953_A+B - 6/src/Main.java
@@ -1,22 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int T = Integer.parseInt(br.readLine());
 
-        while(true){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            if(A == 0 && B == 0){
-                break;
-            }
+        for(int t = 0; t < T; t++){
+            String num = br.readLine();
+            String[] nums= num.split(",");
+
+            int A = Integer.parseInt(nums[0]);
+            int B = Integer.parseInt(nums[1]);
             System.out.println(A+B);
         }
-
     }
 }
