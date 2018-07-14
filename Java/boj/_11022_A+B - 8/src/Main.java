b67b0d7c6fa81c63b9dc207d3448a4fe940e955a
jinhyeok <persona5173@gmail.com>
2018-07-14 23:37:08 +0900

    Complete: java boj 11022 A+B - 8

---
+++ Java/boj/_11022_A+B - 8/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int t = 1; t < T+1; t++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int sum = A+B;
+
+            System.out.println("Case #"+t+": "+ A + " + " + B+" = " + sum );
+        }
+
+    }
+}
