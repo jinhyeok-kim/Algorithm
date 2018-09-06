e7afdd6ed41678ae1bbaacd6e525c49d5a2bd6e0
jinhyeok <persona5173@gmail.com>
2018-09-06 23:26:52 +0900

    Complete: java boj 3047 ABC

---
+++ Java/boj/_3047_ABC/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] num = new int[3];
+
+        int n = 0;
+        while(st.hasMoreElements()){
+            num[n++] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+
+        char[] alpha = br.readLine().toCharArray();
+
+
+        for(int i = 0; i < 3; i++){
+            System.out.print(num[alpha[i]-65] + " ");
+        }
+
+
+
+
+    }
+}
