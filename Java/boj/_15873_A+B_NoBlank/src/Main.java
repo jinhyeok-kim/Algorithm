e696c77c472ba0c70f6d2dd8e9a4ed76f0a2c87d
jinhyeok <persona5173@gmail.com>
2018-09-15 23:55:21 +0900

    Complete: java boj 15873 공백 없는 A+B

---
+++ Java/boj/_15873_A+B_NoBlank/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] num = br.readLine().toCharArray();
+
+        int a = 0;
+        int b = 0;
+
+        if(num.length == 4){
+            a =10;
+            b =10;
+        }else if(num.length == 3){
+            if(num[1]-48 == 0){
+                a = 10;
+                b = (int)num[2] - 48;
+            }else if(num[2]-48 == 0){
+                a = (int)num[0] - 48;
+                b= 10;
+            }
+        }else{
+            a = (int)num[0] - 48;
+            b = (int)num[1] - 48;
+        }
+
+        int sum = a + b;
+
+        System.out.println(sum);
+
+    }
+}
