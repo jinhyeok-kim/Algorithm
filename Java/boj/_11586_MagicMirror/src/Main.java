eed900e246d993d821fb7691e070f6cdf9c0ff94
jinhyeok <persona5173@gmail.com>
2018-09-16 22:52:17 +0900

    Complete: java boj 11586 지영 공주님의 마법 거울

---
+++ Java/boj/_11586_MagicMirror/src/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        char[][] mirror = new char[N][N];
+
+        for(int i = 0; i < N; i++){
+            mirror[i] = br.readLine().toCharArray();
+        }
+        int K = Integer.parseInt(br.readLine());
+
+        if(K == 1){
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    System.out.print(mirror[i][j]);
+                }
+                System.out.println();
+            }
+        }else if(K == 2){
+            for(int i = 0; i < N; i++){
+                for(int j = N-1; j >= 0; j--){
+                    System.out.print(mirror[i][j]);
+                }
+                System.out.println();
+            }
+
+        }else{
+            for(int i = N-1; i >= 0; i--){
+                for(int j = 0; j < N; j++){
+                    System.out.print(mirror[i][j]);
+                }
+                System.out.println();
+            }
+        }
+
+
+    }
+}
