e1b60ccd57c6502d912ce88a3d886b1c84812352
jinhyeok <persona5173@gmail.com>
2018-10-22 23:37:36 +0900

    Complete: java boj 10093 숫자

---
+++ Java/boj/_10093_Number/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        if(A == B){
+            System.out.println(0);
+        }else {
+            if(A > B){
+                long temp = A;
+                A = B;
+                B = temp;
+
+            }
+            System.out.println(B - A - 1);
+        }
+
+        for(long i = A+1; i < B; i++){
+            System.out.print(i + " ");
+        }
+
+    }
+}
