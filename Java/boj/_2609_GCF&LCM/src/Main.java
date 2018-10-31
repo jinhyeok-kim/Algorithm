edb4c7f314c4672c1bdf79fc039ea388047d4ac6
jinhyeok <persona5173@gmail.com>
2018-10-31 13:36:19 +0900

    Complete: java boj 2609 최대공약수와 최소공배수

---
+++ Java/boj/_2609_GCF&LCM/src/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int tmp1 = A;
+        int tmp2 = B;
+        if(A<B){
+            int temp = A;
+            A = B;
+            B = temp;
+        }
+        int r = 1;
+
+        while(r>0){
+            r = A % B;
+            A = B;
+            B = r;
+        }
+
+        System.out.println(A);
+        System.out.println(tmp1*tmp2/A);
+
+
+
+
+
+    }
+}
