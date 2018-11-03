d46c84b33fdcbc753b00bb808d3a9194208625f1
jinhyeok <persona5173@gmail.com>
2018-11-03 23:35:26 +0900

    Complete: java boj 10797 10부제

---
+++ Java/boj/_10797_10System/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int count = 0;
+        for(int i = 0; i < 5; i++){
+            int carNum = Integer.parseInt(st.nextToken());
+
+            if(num == carNum){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
