22963d9f84201d1aad9751bafee321865680a7e2
jinhyeok <persona5173@gmail.com>
2018-11-07 23:50:36 +0900

    Incomplete: java boj 10773 좌표 정렬하기

---
+++ Java/boj/_11650_AlignCoordinates/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int x[] = new int[N];
+        int y[] = new int[N];
+
+        int n[] = new int[N];
+
+        for(int i = 0; i< N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i<N; i++){
+
+        }
+
+    }
+}
