868e113992cd8a946ae9a640f17134d13f5d10a9
jinhyeok <persona5173@gmail.com>
2018-11-10 23:25:48 +0900

    Complete: java boj 10940 BASE16 인코딩

---
+++ Java/boj/_10940_BASE16Encoding/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+
+
+        System.out.println(asciiToHex(s));
+
+
+
+
+
+    }
+
+    private static String asciiToHex(String asciiStr) {
+        char[] chars = asciiStr.toCharArray();
+        StringBuilder hex = new StringBuilder();
+        for (char ch : chars) {
+            hex.append(Integer.toHexString((int) ch));
+        }
+
+        return hex.toString().toUpperCase();
+    }
+
+
+}
