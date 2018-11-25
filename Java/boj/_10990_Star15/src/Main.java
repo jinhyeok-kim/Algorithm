5f1745a839c513f518c255635ffc092a50d3bc36
jinhyeok <persona5173@gmail.com>
2018-11-25 23:02:53 +0900

    Complete: java boj 10990 별 찍기 - 15

--- Java/boj/_10990_Star15/src/Main.java
+++ Java/boj/_10990_Star15/src/Main.java
@@ -8,15 +8,16 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        for(int n = 0; n < 2*N-1; n++){
-            if(n < N){
-                for(int i = 0; i <= n; i++){
-                    System.out.print("*");
-                }
-            }else{
-                for(int i = n-N; i < N-1; i++){
-                    System.out.print("*");
-                }
+        for(int n = 0; n < N; n++){
+            for(int i = N; i > n+1; i--){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for(int i = 0; i < 2*n-1; i++){
+                System.out.print(" ");
+            }
+            if( n != 0 ) {
+                System.out.print("*");
             }
             System.out.println();
         }
