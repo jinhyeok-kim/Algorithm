16ddbe624caf32917d0599096b7023ebb03640c5
jinhyeok <persona5173@gmail.com>
2018-11-29 23:42:18 +0900

    Complete: java boj 10995 별 찍기 - 20

---
+++ Java/boj/_10995_Star20/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int n = 0; n < N; n++){
+            if(n%2 == 0) {
+                for (int i = 0; i < N; i++) {
+                    System.out.print("* ");
+                }
+            }else{
+                for (int i = 0; i < N; i++) {
+                    System.out.print(" *");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
