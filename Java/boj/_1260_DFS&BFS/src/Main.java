7e9e9c90c6e0a407daf35f7bc3d4a6105417ce3b
jinhyeok <persona5173@gmail.com>
2019-01-27 19:33:41 +0900

    Modify: java boj 1260 DFS와 BFS

--- Java/boj/_1260_DFS&BFS/src/Main.java
+++ Java/boj/_1260_DFS&BFS/src/Main.java
@@ -1,65 +1,82 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static int n, m, v;
-    static int[][] graph;
-    static boolean check[];
-    public static void main(String[] args) throws IOException {
+    static ArrayList<Integer>[] adj;
+    static boolean[] isvisited;
+    static BufferedWriter bw;
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        v = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
 
-        graph = new int[n + 1][n + 1];
+        adj = new ArrayList[N+1];
+        isvisited = new boolean[N+1];
 
-        for(int i = 0; i < m; i++){
+        for(int i = 1; i < N+1; i++){
+            adj[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            graph[x][y] = 1;
-            graph[y][x] = 1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            adj[a].add(b);
+            adj[b].add(a);
         }
 
-        check = new boolean[n+1];
-        dfs(v);
-        System.out.println();
-        check = new boolean[n+1];
-        bfs(v);
+        for(int i = 1; i < adj.length; i++){
+            Collections.sort(adj[i]);
+        }
+
+        dfs(V);
+
+        bw.flush();
+        bw.newLine();
+
+        isvisited = new boolean[N+1];
+
+        bfs(V);
+
+        bw.flush();
+
+
 
     }
 
-    public static void dfs(int v){
-        System.out.print(v + " ");
-        check[v] = true;
-        for(int i = 1; i <= n; i++){
-            if(graph[v][i] == 1 && !check[i]){
-                dfs(i);
-            }
-        }
-    }
+    private static void bfs(int v) throws IOException {
+        Queue<Integer> queue = new LinkedList<>();
 
-    public static void bfs(int v){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(v);
-        check[v] = true;
+        queue.add(v);
+        isvisited[v] = true;
 
-        while(!q.isEmpty()){
-            int next = q.poll();
-            System.out.print(next + " ");
-            for(int i = 1; i <= n; i++){
-                if(graph[next][i] == 1 && !check[i]){
-                    q.add(i);
-                    check[i] = true;
+        while(!queue.isEmpty()){
+            v = queue.poll();
+            bw.write(v + " ");
+            for(int i : adj[v]){
+                if(!isvisited[i]){
+                    queue.add(i);
+                    isvisited[i] = true;
                 }
             }
+        }
+        
+
+    }
+
+    private static void dfs(int v) throws IOException {
+        if(isvisited[v]) return;
+        isvisited[v] = true;
+        bw.write(v + " ");
+
+        for(int i : adj[v]){
+            dfs(i);
         }
     }
 }
