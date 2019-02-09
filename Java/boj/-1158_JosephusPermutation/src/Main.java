fe26c4872faed37c057379b5cefa1f7432a6defb
jinhyeok <persona5173@gmail.com>
2019-02-10 00:45:47 +0900

    Complete: java boj 1158 조세퍼스 순열

--- Java/boj/-1158_JosephusPermutation/src/Main.java
+++ Java/boj/-1158_JosephusPermutation/src/Main.java
@@ -1,5 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        LinkedList<Integer> ll = new LinkedList<>();
+
+
+       for(int i = 1; i <= N; i++){
+           ll.add(i);
+       }
+
+       int count = M - 1;
+        System.out.print("<");
+       while(true){
+
+           if(ll.size() == 1){
+               System.out.print(ll.get(count));
+           }else {
+               System.out.print(ll.get(count) + ", ");
+           }
+           ll.remove(count);
+           if(ll.isEmpty()){
+               break;
+           }
+
+           count +=  M - 1;
+           count = count %ll.size();
+
+       }
+
+        System.out.println(">");
+
     }
 }
